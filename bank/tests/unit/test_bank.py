1e454bc7daa7e3d72b76b1136bcfcc0e0f463e0d
glen <glen84_dr@hotmail.com>
2026-06-15 17:16:40 +0200

    starting Bank tests

---
+++ bank/tests/unit/test_bank.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from users.models import UserRole
+from bank.tests.factory_models.models import BankFactory, CompanyFactory, CustomUserFactory
+
+class BankTestCase(TestCase):
+    def setUp(self):
+        ...
+
+    def test_bank_creation(self):
+        ...
+
+    def test_bank_update(self):
+        ...
+
+    def test_bank_delete(self):
+        ...
